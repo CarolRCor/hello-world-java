2aa4e9152b03aa49c0308275584bc9961ed79253
CarolRCor <carolrcordeiro@gmail.com>
2025-08-11 19:56:22 -0300

    Adiciona o metodo holaquetal na branch feature/hola-que-tal

--- hello.java
+++ hello.java
@@ -3,6 +3,10 @@ public class hello {
     public static void helloworld() {
         System.out.println("Hello world");
     }
+
+    public static void holaquetal() {
+        System.out.println("Hola que tal");
+    }
     public static void main(String[] args) {
         helloworld();
     }
