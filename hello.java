bfc5867e993e17a072c3684b43b6fca8f8516c39
CarolRCor <carolrcordeiro@gmail.com>
2025-08-11 19:49:09 -0300

    Adiciona o metodo olaMundo na branch feature/ola-mundo

--- hello.java
+++ hello.java
@@ -3,6 +3,10 @@ public class hello {
     public static void helloworld() {
         System.out.println("Hello world");
     }
+    public static void olaMundo() {
+        System.out.println("Ola mundo");
+    }
+
     public static void main(String[] args) {
         helloworld();
     }
